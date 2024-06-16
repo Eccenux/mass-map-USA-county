c5d86fed0badc43b2a5a35416ef6c446563daf86
eccenux <eccenux+git@gmail.com>
2024-06-16 11:05:28 +0200

    working version

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-const { login, getCsrfToken, fileExists, uploadFile } = require("./api-helper");
+// const { login, getCsrfToken, fileExists, uploadFile } = require("./api-helper");
 
-const filePath = 'Map_of_New_York.svg'; // Change to your file path
+const { MwApi } = require("./mw-api");
+
+const apiUrl = 'https://commons.wikimedia.org/w/api.php';
+const { USERNAME, PASSWORD } = require('./bot.config');
+
+let api = new MwApi(apiUrl);
+
+// TODO:
+// Loop over named ids in the map (or maybe predefined list...).
+// Change current id in `<use xlink:href="#Rensselaer" stroke="none" fill="red" />`
+// If exists upload new file as `Map_of_New_York_highlighting_${id}_County.svg`
+// When missing add to missing list.
+// Upon error stop (at least for now).
+// Finally dump list of missing files.
+// Show stats: missing count, uploaded count.
 
 (async () => {
 	try {
-		const { cookie } = await login();
-		const csrfToken = await getCsrfToken(cookie);
+		// estabilish session
+		await api.login(USERNAME, PASSWORD);
 
-		const commonsName = 'Map_of_New_York_highlighting_Putnam_County.svg';
-		if (await fileExists(commonsName, cookie)) {
-			await uploadFile(commonsName, filePath, csrfToken, cookie);
-			console.log('File uploaded successfully.');
+		let srcFilePath = 'Map_of_New_York.svg';
+		let destFileName = 'Map_of_New_York_highlighting_Putnam_County.svg';
+		let summary = "fix rendering (remove clipping)";
+		if (await api.fileExists(destFileName)) {
+			await api.upload(srcFilePath, destFileName, summary);
+			console.log(`File uploaded as: ${destFileName}.`);
 		} else {
-			console.log('File does not exist on Commons.');
+			console.warn(`File:${destFileName} does not exist on Commons.');
 		}
 	} catch (error) {
 		console.error('Error:', error);
---
+++ mw-api.js
@@ -0,0 +1,167 @@
+/*  
+    Auth and upload files.
+ 
+    MediaWiki API Demos
+    Demo of `Upload` module: Sending post request to upload a file directly.
+	Note that the `request` module is able to estabilish a session with cookies
+	(keeps cookies set byt the server in an initial request).
+
+    MIT license
+*/
+const fs = require('fs');
+const request = require('request').defaults({jar: true});
+
+class MwApi {
+	constructor(apiUrl = 'https://commons.wikimedia.org/w/api.php') {
+		this.apiUrl = apiUrl
+	}
+
+	/**
+	 * Authenticate and get edit token.
+	 * 
+	 * Use of main account for login is not supported.
+	 * Obtain credentials for lgname & lgpassword via:
+	 * https://test.wikipedia.org/wiki/Special:BotPasswords
+	 * 
+	 * @param {String} username bot name (lgname)
+	 * @param {String} password bot password (lgpassword)
+	 */
+	async login(username, password) {
+		const loginToken = await this.getLoginToken();
+		const loginData = await this.loginRequest(loginToken, username, password);
+		if (loginData?.login?.result === 'Success') {
+			console.log(`Auth as ${username} succesfull.`);
+		} else {
+			console.error(`Auth as ${username} failed.`, JSON.stringify(loginData));
+			throw "Failed auth";
+		}
+	
+		await this.saveCsrfToken();
+	}
+
+	/** @private Step 1: GET request to fetch login token and init session. */
+	getLoginToken() {
+		let params = {
+			action: "query",
+			meta: "tokens",
+			type: "login",
+			format: "json"
+		};
+
+		return new Promise((resolve, reject) => {
+			request.get({ url: this.apiUrl, qs: params }, (error, res, body) => {
+				if (error) {
+					reject(error);
+					return;
+				}
+				var data = JSON.parse(body);
+				resolve(data.query.tokens.logintoken);
+			});
+		})
+	}
+	/** @private Step 2: POST request to log in. */
+	loginRequest(loginToken, username, password) {
+		let params = {
+			action: "login",
+			lgname: username,
+			lgpassword: password,
+			lgtoken: loginToken,
+			format: "json"
+		};
+
+		return new Promise((resolve, reject) => {
+			request.post({ url: this.apiUrl, form: params }, (error, res, body) => {
+				if (error) {
+					reject(error);
+					return;
+				}
+				var data = JSON.parse(body);
+				resolve(data);
+			});
+		});
+	}
+	/** @private Step 3: Fetch and save internaly a CSRF token for edits and changes. */
+	saveCsrfToken() {
+		let params = {
+			action: "query",
+			meta: "tokens",
+			format: "json"
+		};
+
+		return new Promise((resolve, reject) => {
+			request.get({ url: this.apiUrl, qs: params }, (error, res, body)  => {
+				if (error) {
+					reject(error);
+					return;
+				}
+				let data = JSON.parse(body);
+				this.csrfToken = data.query.tokens.csrftoken;
+				resolve(data);
+			});
+		});
+	}
+
+	/** Check if file exists on the server. */
+	fileExists(destFileName) {
+		let params = {
+			action: "query",
+			titles: `File:${destFileName}`,
+			format: "json"
+		};
+
+		return new Promise((resolve, reject) => {
+			request.get({ url: this.apiUrl, qs: params }, (error, res, body)  => {
+				if (error) {
+					reject(error);
+					return;
+				}
+				let data = JSON.parse(body);
+				const pages = data.query.pages;
+				resolve(!pages.hasOwnProperty("-1"));
+			});
+		});
+	}	
+
+	/** Upload a file directly. */
+	upload(srcFilePath, destFileName, summary = "quick edit") {
+		let params = {
+			action: "upload",
+			filename: destFileName,
+			ignorewarnings: "1",
+			token: this.csrfToken,
+			comment: summary,
+			format: "json"
+		};
+
+		var file = {
+			file: fs.createReadStream(srcFilePath)
+		};
+
+		var formData = Object.assign( {}, params, file );
+
+		return new Promise((resolve, reject) => {
+			request.post({ url: this.apiUrl, formData: formData }, (error, res, body) => {
+				if (error) {
+					reject(error);
+					return;
+				}
+				let data = JSON.parse(body);
+				if (data.error) {
+					reject(data.error);
+					return;
+				}
+				else if (data.upload.result === "Success"){
+					resolve(data.upload.result)
+				}
+				else {
+					reject(data.upload.result)
+				}
+			});
+		});
+	}
+
+}
+
+module.exports = {
+	MwApi,
+}
