962e8e06ec57598302fd8f585a1710b621b928a4
eccenux <eccenux+git@gmail.com>
2024-06-19 20:47:42 +0200

    North_Dakota

--- img/usa_ids.js
+++ img/usa_ids.js
@@ -1,6 +1,5 @@
 /**
- * Find state ids while the SVg is open.
- * 
+ * Find state ids while the SVG is open.
  * Skips numeric ids. The rest should be counties.
  */
 function findIds() {
@@ -13,20 +12,78 @@ function findIds() {
 	console.log(ids.length);
 	copy(ids);
 }
+function whResize() {
+	var divide = 3.5;
 
-let stateName = 'Connecticut';
+	var svg = document.querySelector('svg');
+	var width = svg.getAttribute('width');
+	var height = svg.getAttribute('height');
+	function resize(x) {
+		return (Number.parseFloat(x) / d).toFixed(2);
+	}
+	width=resize(width);
+	height=resize(height);
+	console.log(`width="${width}" height="${height}"`)
+}
+
+let stateName = 'North_Dakota';
 let srcFilePath = `img/Map_of_${stateName}.svg`;
 let destFileTemplate = (id)=>`Map_of_${stateName}_highlighting_${id}_County.svg`;
 
 const counties = [
-	"Fairfield",
-	"Hartford",
-	"Litchfield",
-	"Middlesex",
-	"New_Haven",
-	"New_London",
-	"Tolland",
-	"Windham"
+	"Adams",
+	"Barnes",
+	"Benson",
+	"Billings",
+	"Bottineau",
+	"Bowman",
+	"Burke",
+	"Burleigh",
+	"Cass",
+	"Cavalier",
+	"Dickey",
+	"Divide",
+	"Dunn",
+	"Eddy",
+	"Emmons",
+	"Foster",
+	"Golden_Valley",
+	"Grand_Forks",
+	"Grant",
+	"Griggs",
+	"Hettinger",
+	"Kidder",
+	"LaMoure",
+	"Logan",
+	"McHenry",
+	"McIntosh",
+	"McKenzie",
+	"McLean",
+	"Mercer",
+	"Morton",
+	"Mountrail",
+	"Nelson",
+	"Oliver",
+	"Pembina",
+	"Pierce",
+	"Ramsey",
+	"Ransom",
+	"Renville",
+	"Richland",
+	"Rolette",
+	"Sargent",
+	"Sheridan",
+	"Sioux",
+	"Slope",
+	"Stark",
+	"Steele",
+	"Stutsman",
+	"Towner",
+	"Traill",
+	"Walsh",
+	"Ward",
+	"Wells",
+	"Williams"
 ];
 
 module.exports = {
