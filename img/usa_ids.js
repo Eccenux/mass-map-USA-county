bca0cb97bafc1c03e68fb84269fef6cbc1bb204e
eccenux <eccenux+git@gmail.com>
2024-06-19 20:59:32 +0200

    Delaware

--- img/usa_ids.js
+++ img/usa_ids.js
@@ -25,65 +25,16 @@ function whResize() {
 	height=resize(height);
 	console.log(`width="${width}" height="${height}"`)
 }
+// findIds();whResize();
 
-let stateName = 'South_Dakota';
+let stateName = 'Delaware';
 let srcFilePath = `img/Map_of_${stateName}.svg`;
 let destFileTemplate = (id)=>`Map_of_${stateName}_highlighting_${id}_County.svg`;
 
 const counties = [
-	"Adams",
-	"Barnes",
-	"Benson",
-	"Billings",
-	"Bottineau",
-	"Bowman",
-	"Burke",
-	"Burleigh",
-	"Cass",
-	"Cavalier",
-	"Dickey",
-	"Divide",
-	"Dunn",
-	"Eddy",
-	"Emmons",
-	"Foster",
-	"Golden_Valley",
-	"Grand_Forks",
-	"Grant",
-	"Griggs",
-	"Hettinger",
-	"Kidder",
-	"LaMoure",
-	"Logan",
-	"McHenry",
-	"McIntosh",
-	"McKenzie",
-	"McLean",
-	"Mercer",
-	"Morton",
-	"Mountrail",
-	"Nelson",
-	"Oliver",
-	"Pembina",
-	"Pierce",
-	"Ramsey",
-	"Ransom",
-	"Renville",
-	"Richland",
-	"Rolette",
-	"Sargent",
-	"Sheridan",
-	"Sioux",
-	"Slope",
-	"Stark",
-	"Steele",
-	"Stutsman",
-	"Towner",
-	"Traill",
-	"Walsh",
-	"Ward",
-	"Wells",
-	"Williams"
+	"Kent",
+	"New_Castle",
+	"Sussex"
 ];
 
 module.exports = {
