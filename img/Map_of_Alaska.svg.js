f024899f797de12ebe440ee24173377c46fd7131
eccenux <eccenux+git@gmail.com>
2024-06-25 22:06:06 +0200

    full Alaska mapping

--- img/Map_of_Alaska.svg.js
+++ img/Map_of_Alaska.svg.js
@@ -3,7 +3,64 @@ let stateName = 'Alaska';
 let srcFilePath = 'img/Map_of_' + stateName + '.svg';
 let destFileTemplate = (id) => 'Map_of_' + stateName + '_highlighting_' + id + '_County.svg';
 
-const counties = ["Aleutians_East","Anchorage_Municipality","Bethel","Bristol_Bay","Denali","Dillingham","Fairbanks_North_Star","Haines","Juneau","Kenai_Peninsula","Kodiak_Island","Lake_and_Peninsula","Matanuska-Susitna","Nome","North_Slope","Northwest_Arctic","Prince_of_Wales-Outer_Ketchikan","Sitka","Skagway-Hoonah-Angoon","Southeast_Fairbanks","Valdez-Cordova","Wade_Hampton","Wrangell-Petersburg","Yakutat","Yukon-Koyukuk"];
+//"Map of Alaska highlighting Skagway City and Borough.svg", // arrow (small)
+
+let mappingTemplate = (id, group)=>`Map_of_Alaska_highlighting_${id}_${group}.svg`;
+let mapping = {
+	"Borough": [
+		"Ketchikan_Gateway",//
+		"Kodiak_Island",//
+		"Matanuska-Susitna",//
+		"Northwest_Arctic",//
+		"Kenai_Peninsula",//
+		"Lake_and_Peninsula",//
+		"Fairbanks_North_Star",//
+		"Haines",//
+		"Denali",//
+		"North_Slope",//
+		"Aleutians_East",//
+		"Bristol_Bay",//
+	],
+	"City_and_Borough": [
+		"Yakutat",//
+		"Juneau",//
+		"Wrangell",//
+		"Sitka",//
+	],
+	"Census_Area": [
+		"Petersburg",//
+		"Kusilvak",//
+		"Chugach",//
+		"Dillingham",//
+		"Bethel",//
+		"Southeast_Fairbanks",//
+		"Aleutians_West",//
+		"Copper_River",//
+		"Hoonah-Angoon",//
+		"Yukon-Koyukuk",//
+		"Valdez-Cordova",//
+		"Skagway-Hoonah-Angoon",//
+		"Prince_of_Wales-Hyder",//
+		"Prince_of_Wales-Outer_Ketchikan",//
+		"Nome",//
+		"Wrangell-Petersburg",//
+	],
+};
+
+let counties = [
+	{id: 'Anchorage_Municipality', destFileName: 'Map_of_Alaska_highlighting_Anchorage_Municipality.svg'},//
+];
+
+for (const group in mapping) {
+	if (Object.hasOwnProperty.call(mapping, group)) {
+		const ids = mapping[group];
+		for (const id of ids) {
+			let destFileName = mappingTemplate (id, group);
+			counties.push({id, destFileName});
+		}
+	}
+}
+// console.log(counties);
 
 module.exports = {
 	counties,
