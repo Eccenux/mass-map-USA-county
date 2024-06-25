3ae48d77f29d1df32ddf44637252c7f2bc052519
eccenux <eccenux+git@gmail.com>
2024-06-25 02:41:55 +0200

    time info and delay for limit

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ const mapSpecs = [
 	// "./img/Map_of_New_Jersey.svg.js",
 	// "./img/Map_of_Utah.svg.js",
 
-	"./img/Map_of_Oregon.svg.js",
-	"./img/Map_of_Washington.svg.js",
-	"./img/Map_of_South_Carolina.svg.js",
-	"./img/Map_of_West_Virginia.svg.js",
-	"./img/Map_of_Montana.svg.js",
-	"./img/Map_of_Colorado.svg.js",
+	// "./img/Map_of_Oregon.svg.js",
+	// "./img/Map_of_Washington.svg.js",
+	// "./img/Map_of_South_Carolina.svg.js",
+	// "./img/Map_of_West_Virginia.svg.js",
+	// "./img/Map_of_Montana.svg.js",
+	// "./img/Map_of_Colorado.svg.js",
 	"./img/Map_of_Oklahoma.svg.js",
 	"./img/Map_of_Mississippi.svg.js",
 	"./img/Map_of_Ohio.svg.js",
@@ -46,10 +46,18 @@ const mapSpecs = [
 		total.error = [...total.error, ...error];
 		total.done += done;
 		total.other += other;
+		info(total, false);
 	}
 	info(total);
 })();
 
+function sleep(s) {
+	let minutes = Math.floor(s / 60);
+	let seconds = s % 60;
+	let info = minutes > 0 ? `${minutes}:${seconds} [min:s]` : `${s} [s]`;
+	console.log(`Waiting... ${info}`);
+	return new Promise(resolve => setTimeout(resolve, s * 1000));
+}
 
 // auth
 async function auth() {
@@ -68,15 +76,14 @@ async function run(options) {
 	console.log("Upload of: ", {srcFilePath, size:counties.length});
 
 	let summary = "fix rendering (remove clipping)";
+	// extra seconds needed for limits...
+	const waitSec = 9;
 
 	// Loop over named ids in the map (or maybe predefined list...).
 	let done = [];
 	let missing = [];
 	let error = [];
-	// counties = [
-	// 	"Albany",
-	// 	"Allegany",
-	// ]		
+	const startTime = Date.now();
 	for (const county of counties) {
 		let id = '';
 		let destFileName = '';
@@ -103,27 +110,34 @@ async function run(options) {
 		} catch (ex) {
 			console.error(`Unable to upload File:${destFileName}.`, ex);
 			error.push({id, destFileName});
+			await sleep(30); // extra
 			// Upon error stop (at least for now).
 			// break;
 		}
+		await sleep(waitSec);
 	}
+	const endTime = Date.now();
+	const elapsed = (endTime - startTime)/1000;
+	console.log(`Elapsed time: ${elapsed.toFixed(1)} [seconds]`);
+	console.log(`Records per minute: ${(counties.length / elapsed * 60.0).toFixed(2)}`);
+
 	let other = counties.length - done.length - missing.length;
 	let results = {done:done.length, missing, error, other};
 	info(results);
 	return results;
 }
 
-function info(results) {
+function info(results, verbose=true) {
 	let {done, missing, error, other} = results;
 
 	// Finally dump list of missing files.
-	if (missing.length) {
+	if (verbose && missing.length) {
 		console.warn("Missing:");
 		missing.forEach((item) => {
 			console.warn(item);
 		});
 	}
-	if (error.length) {
+	if (verbose && error.length) {
 		console.warn("Error:");
 		error.forEach((item) => {
 			console.error(item);
