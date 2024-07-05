84080d93f4eeef80074584c00027eb7896458c34
eccenux <eccenux+git@gmail.com>
2024-07-05 11:10:23 +0200

    virginia half done

--- missing.js
+++ missing.js
@@ -1,54 +1,55 @@
-// repeat
-// Uploaded: 811; missing: 2; other: 6
-let extraMapping2 = {
-	"./img/Map_of_Georgia.svg.js": [
-		{id: 'Telfair', destFileName: 'Map_of_Georgia_highlighting_Telfair_County.svg'},
-	],
-	"./img/Map_of_Illinois.svg.js": [
-		{id: 'De_Witt', destFileName: 'Map of Illinois highlighting DeWitt County.svg'},
-		{id: 'Saint_Clair', destFileName: 'Map of Illinois highlighting St. Clair County.svg'},
-	],
-	"./img/Map_of_South_Dakota.svg.js": [
-		{id: 'Shannon', destFileName: 'Map of South Dakota highlighting Oglala Lakota County.svg'},
-	],
-	"./img/Map_of_New_York.svg.js": [
-		{id: 'Saint_Lawrence', destFileName: 'Map of New York highlighting St. Lawrence County.svg'},
-	],
-}
-
-// done ✅
-let missing = [
-	{id: 'Saint_Louis_City', 		destFileName: 'Map of Missouri highlighting Saint Louis City.svg'},
-	{id: 'Saint_Charles', 	 		destFileName: 'Map of Missouri highlighting St. Charles County.svg'},
-	{id: 'Sainte_Genevieve', 		destFileName: 'Map of Missouri highlighting Ste. Genevieve County.svg'},
-
-	{id: 'Larue', 					destFileName: 'Map of Kentucky highlighting LaRue County.svg'},
-
-	{id: 'Saint_Clair',             destFileName: 'Map of Michigan highlighting St. Clair County.svg'}
-]
-// extra mapping for 3 states (non-standard names)
-let extraMapping = {
-	"./img/Map_of_Missouri.svg.js": [
-		{id: 'Saint_Louis_City', 		destFileName: 'Map of Missouri highlighting Saint Louis City.svg'},
-		{id: 'Saint_Charles', 	 		destFileName: 'Map of Missouri highlighting St. Charles County.svg'},
-		{id: 'Sainte_Genevieve', 		destFileName: 'Map of Missouri highlighting Ste. Genevieve County.svg'},
-	],
-	"./img/Map_of_Kentucky.svg.js": [
-		{id: 'Larue', 					destFileName: 'Map of Kentucky highlighting LaRue County.svg'},
-	],
-	"./img/Map_of_Michigan.svg.js": [
-		{id: 'Saint_Clair',             destFileName: 'Map of Michigan highlighting St. Clair County.svg'}
-	],
-};
-
-/*
-False-Error (error, but OK):
-{id: '✅Itawamba',               destFileName: 'Map_of_Mississippi_highlighting_Itawamba_County.svg'}
-{id: '✅Lawrence',               destFileName: 'Map_of_Mississippi_highlighting_Lawrence_County.svg'}
-{id: '✅Tippah',                 destFileName: 'Map_of_Mississippi_highlighting_Tippah_County.svg'}
-{id: '✅Defiance', 				destFileName: 'Map_of_Ohio_highlighting_Defiance_County.svg'}
-{id: '✅Boyd', 					destFileName: 'Map_of_Nebraska_highlighting_Boyd_County.svg'}
-
-Uploaded: 953; missing: 1; other: 8
-Uploaded: 244; missing: 0; other: 1
-*/
+// todo?
+var missing = [
+	{id: 'Alexandria', destFileName: 'Map of Virginia highlighting Alexandria County.svg'},
+	{id: 'Bristol', destFileName: 'Map of Virginia highlighting Bristol County.svg'},
+	{id: 'Buena Vista', destFileName: 'Map of Virginia highlighting Buena Vista County.svg'},
+	{id: 'Charles', destFileName: 'Map of Virginia highlighting Charles County.svg'},
+	{id: 'Charlottesville', destFileName: 'Map of Virginia highlighting Charlottesville County.svg'},
+	{id: 'Chesapeake', destFileName: 'Map of Virginia highlighting Chesapeake County.svg'},
+	{id: 'Colonial Heights', destFileName: 'Map of Virginia highlighting Colonial Heights County.svg'},
+	{id: 'Covington', destFileName: 'Map of Virginia highlighting Covington County.svg'},
+	{id: 'Danville', destFileName: 'Map of Virginia highlighting Danville County.svg'},
+	{id: 'Emporia', destFileName: 'Map of Virginia highlighting Emporia County.svg'},
+	{id: 'Falls Church', destFileName: 'Map of Virginia highlighting Falls Church County.svg'},
+	{id: 'Fredericksburg', destFileName: 'Map of Virginia highlighting Fredericksburg County.svg'},
+	{id: 'Galax', destFileName: 'Map of Virginia highlighting Galax County.svg'},
+	{id: 'Hampton', destFileName: 'Map of Virginia highlighting Hampton County.svg'},
+	{id: 'Harrisonburg', destFileName: 'Map of Virginia highlighting Harrisonburg County.svg'},
+	{id: 'Hopewell', destFileName: 'Map of Virginia highlighting Hopewell County.svg'},
+	{id: 'James', destFileName: 'Map of Virginia highlighting James County.svg'},
+	{id: 'Lexington', destFileName: 'Map of Virginia highlighting Lexington County.svg'},
+	{id: 'Lynchburg', destFileName: 'Map of Virginia highlighting Lynchburg County.svg'},
+	{id: 'Manassas', destFileName: 'Map of Virginia highlighting Manassas County.svg'},
+	{id: 'Manassas Park', destFileName: 'Map of Virginia highlighting Manassas Park County.svg'},
+	{id: 'Martinsville', destFileName: 'Map of Virginia highlighting Martinsville County.svg'},
+	{id: 'Newport News', destFileName: 'Map of Virginia highlighting Newport News County.svg'},
+	{id: 'Norfolk', destFileName: 'Map of Virginia highlighting Norfolk County.svg'},
+	{id: 'Norton', destFileName: 'Map of Virginia highlighting Norton County.svg'},
+	{id: 'Petersburg', destFileName: 'Map of Virginia highlighting Petersburg County.svg'},
+	{id: 'Poquoson', destFileName: 'Map of Virginia highlighting Poquoson County.svg'},
+	{id: 'Portsmouth', destFileName: 'Map of Virginia highlighting Portsmouth County.svg'},
+	{id: 'Radford', destFileName: 'Map of Virginia highlighting Radford County.svg'},
+	{id: 'Salem', destFileName: 'Map of Virginia highlighting Salem County.svg'},
+	{id: 'Staunton', destFileName: 'Map of Virginia highlighting Staunton County.svg'},
+	{id: 'Suffolk', destFileName: 'Map of Virginia highlighting Suffolk County.svg'},
+	{id: 'Virginia Beach', destFileName: 'Map of Virginia highlighting Virginia Beach County.svg'},
+	{id: 'Waynesboro', destFileName: 'Map of Virginia highlighting Waynesboro County.svg'},
+	{id: 'Williamsburg', destFileName: 'Map of Virginia highlighting Williamsburg County.svg'},
+	{id: 'Winchester', destFileName: 'Map of Virginia highlighting Winchester County.svg'},
+];
+// ✅✅
+var error = [
+	{id: 'Augusta', destFileName: 'Map of Virginia highlighting Augusta County.svg'},
+	{id: 'Bedford', destFileName: 'Map of Virginia highlighting Bedford County.svg'},
+	{id: 'Brunswick', destFileName: 'Map of Virginia highlighting Brunswick County.svg'},
+	{id: 'Fairfax', destFileName: 'Map of Virginia highlighting Fairfax County.svg'},
+	{id: 'Franklin', destFileName: 'Map of Virginia highlighting Franklin County.svg'},
+	{id: 'Frederick', destFileName: 'Map of Virginia highlighting Frederick County.svg'},
+	{id: 'Northampton', destFileName: 'Map of Virginia highlighting Northampton County.svg'},
+	{id: 'Page', destFileName: 'Map of Virginia highlighting Page County.svg'},
+	{id: 'Powhatan', destFileName: 'Map of Virginia highlighting Powhatan County.svg'},
+	{id: 'Richmond', destFileName: 'Map of Virginia highlighting Richmond County.svg'},
+	{id: 'Roanoke', destFileName: 'Map of Virginia highlighting Roanoke County.svg'},
+	{id: 'Scott', destFileName: 'Map of Virginia highlighting Scott County.svg'},
+	{id: 'Wise', destFileName: 'Map of Virginia highlighting Wise County.svg'},
+];
