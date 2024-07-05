e1c69e08075f2c1de48924291880cb834aaca1c3
eccenux <eccenux+git@gmail.com>
2024-07-05 09:25:24 +0200

    +2 done

--- img/Map_of_Nevada.svg.js
+++ img/Map_of_Nevada.svg.js
@@ -3,7 +3,14 @@ let stateName = 'Nevada';
 let srcFilePath = 'img/Map_of_' + stateName + '.svg';
 let destFileTemplate = (id) => 'Map_of_' + stateName + '_highlighting_' + id + '_County.svg';
 
-const counties = ["Carson_City","Churchill","Clark","Douglas","Elko","Esmeralda","Eureka","Humboldt","Lander","Lincoln","Lyon","Mineral","Nye","Pershing","Storey","Washoe","White_Pine"];
+let counties = [
+	{id: 'Carson_City', destFileName: 'Map of Nevada highlighting Carson City.svg'},
+	"Churchill","Clark","Douglas","Elko","Esmeralda","Eureka","Humboldt","Lander","Lincoln","Lyon","Mineral","Nye","Pershing","Storey","Washoe","White_Pine"
+];
+// counties = [
+// 	{id: 'Carson_City', destFileName: 'Map of Nevada highlighting Carson City.svg'},
+// ];
+
 
 module.exports = {
 	counties,
